142398f26ecf1c3b7ef5619ef74075099f48045c
Geovanni Quinteros <ghuertaquinteros@gmail.com>
2026-07-10 02:16:12 -0400

    aporte

--- script.js
+++ script.js
@@ -29,4 +29,50 @@ function validarFormulario() {
     if (mascotas.edad < 0 || isNaN(mascotas.edad)) {
         return "Debe ingresar edad mayor a 0";
     }
+
+    return "";   
 }
+
+function registrarMascota() {
+    let mascota = obtenerDatosMascotas();
+    let error = validarFormulario(mascota);
+
+    if (error !== "") {
+        mostrarMensaje(error);
+        return;
+    }
+
+    mascotas.push(mascota);
+
+    limpiarFormulario();
+    mostrarMensaje("La mascota se ha registrado correctamente");
+    mostrarMascotas();
+
+} 
+
+function mostrarMascotas() {
+    let lista = document.getElementById("listaRegistro");
+    lista.innerHTML = "";
+
+    mascotas.forEach((mascota, index) => {
+        let item = document.createElement("li");
+
+        item.textContent = mascota.nombre +
+            " | Especie: " + mascotas.especie +
+            " | Dueño: " + mascotas.propietario +
+            " | Edad: " + mascotas.edad +
+            " años | Estado: " + mascotas.estado;
+
+        
+    });
+}
+
+function limpiarFormulario() {
+    document.getElementById("mascotaNombre").value = "";
+    document.getElementById("mascoEspecie").value = "";
+    document.getElementById("mascotaPropietario").value = "";
+    document.getElementById("mascotaEdad").value = "";
+
+}
+
+document.getElementById("btnRegistrar").addEventListener("click", registrarMascota);
